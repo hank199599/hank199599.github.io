08ea7afed60f9097b9b93dbeab5947a96cb6b261
hank199599 <36280707+hank199599@users.noreply.github.com>
2020-05-02 20:38:00 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,69 +1,28 @@
-window.addEventListener("load",init);
-
-var nav = document.getElementById("nav");
-var isNavScrolled = false;
-
-function init() {
-    var email = document.getElementById("email");
-    email.addEventListener("click",toggleEmailDialog,false);
-
-    document.addEventListener("scroll",scrollHandler);
-    scrollHandler();
-
-    $(".nav_item").on('click', function (e) {
-        
-        var tag = this.getAttribute("data-page");
-        // $(".nav_item").removeClass("nav_active");
-        // this.classList.add("nav_active");
-      
-        $('html, body').stop().animate({
-            'scrollTop': $(tag).offset().top
-        }, 500, 'swing', function () {
-            window.location.hash = tag;
-        });
-    });
-}
-
-function toggleEmailDialog(){
-    var email_dialog = document.getElementById("email_dialog").style;
-
-    if(email_dialog.display == "block"){
-        email_dialog.display = "none";
-    }
-    else{
-        email_dialog.display = "block";
-    }
-}
-
-function scrollHandler(){
-    var scrollHeight = document.documentElement.scrollTop||document.body.scrollTop;
-
-    if(isNavScrolled && scrollHeight<=1){
-        isNavScrolled = false;
-        // nav.style.boxShadow = "none";
-        nav.style.backgroundColor = "rgba(0,0,0,0)";
-    }
-    else if(!isNavScrolled && scrollHeight>0){
-        isNavScrolled = true;
-        // nav.style.boxShadow = "0px -1px 9px 1px #444";
-        nav.style.backgroundColor = "rgba(0,0,0,    0.25)";
-    }
-
-    onScroll(scrollHeight);
-}
-
-function onScroll(scrollHeight){
-    
-    $(".nav_item").each(function () {
-        var tag = this.getAttribute("data-page");
-        
-        var refElement = $(tag);
-        if (refElement.position().top <= scrollHeight+1 && refElement.position().top + refElement.height() > scrollHeight) {
-            $(".nav_item").removeClass("nav_active");
-            this.classList.add("nav_active");
-        }
-        else{
-            this.classList.remove("nav_active");
-        }
-    });
+var ableToClick = true;
+function buttonClick(){
+  if(!ableToClick) return;
+  ableToClick=false;
+  
+  let splash = document.getElementById("splash");
+  splash.style.width="300px";
+  splash.style.height="300px";
+  
+  setTimeout(function(){
+    splash.style.opacity="0";
+  }, 400);
+  
+  setTimeout(function(){
+    splash.style.transitionDuration="0s";
+  }, 1000);
+  
+  setTimeout(function(){
+    splash.style.width="0";
+    splash.style.height="0";
+    splash.style.opacity="1";
+  }, 1100);
+  
+  setTimeout(function(){
+    ableToClick = true;
+    splash.style.transitionDuration=".5s";
+  }, 1200);
 }
