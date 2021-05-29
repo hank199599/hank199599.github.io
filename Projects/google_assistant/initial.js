08028f16c321d970a8099d3e66572b60a0cda1a2
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2021-05-30 02:32:13 +0800

    [Assistant] link direction update

---
+++ Projects/google_assistant/initial.js
@@ -0,0 +1,2 @@
+display_generator("中文(台灣)")
+model_generator("中文(台灣)")
--- Projects/google_assistant/option_generator.js
+++ Projects/google_assistant/option_generator.js
@@ -27,11 +27,7 @@ for (var i=0;i < display_list.length;i++){
 
     for (var j=0;j<button_list.length;j++){
         option_buttons=option_buttons+'<button class="btn btn-primary btn-link" data-toggle="modal" data-target="#'+button_list[j]+'">'+
-        '<img src="'+detail_dict[button_list[j]]['src']+'"'
-        if (recommand_list.indexOf(button_list[j])!==-1){
-            option_buttons=option_buttons+' data-toggle="tooltip" data-placement="top" title="Google官方推薦" data-container="body"'
-        }
-        option_buttons=option_buttons+'/></button>'
+        '<img src="'+detail_dict[button_list[j]]['src']+'"/></button>'
     }
     option_buttons=option_buttons+'</div>'
 }
@@ -39,5 +35,6 @@ for (var i=0;i < display_list.length;i++){
 document.getElementById('nav_items').innerHTML = '<ul class="nav nav-tabs" data-tabs="tabs">'+nav_options+'</ul>'
 document.getElementById('nav_buttons').innerHTML = option_buttons
 document.getElementById('navbar-brand').innerHTML = language
+model_generator(language)
 }
 
