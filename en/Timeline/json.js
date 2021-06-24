8ffa2220010dc46a24a81138ab3891c17b3ea294
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2021-06-24 23:21:38 +0800

    [EN] add english version page

---
+++ en/Timeline/json.js
@@ -0,0 +1,58 @@
+var contents={
+    "2021/08/02":{
+        "title":"Teaching assistant",
+        "content":"Woman in AI</br>This workshop will teach the AI ​​operating procedures from the perspective of a data scientist.<br> You don't need to have advanced programming skills. </br>In the workshop, senior elder sisters in the industry will serve as mentors and teaching assistants,<br>  leading everyone from basic data processing to how to convert data into computer-calculated data.",
+        "button":{
+            "herf":"https://www.accupass.com/event/2104250103576884283220",
+            "content":"<i class='material-icons'>menu_book</i> ACCUPASS "
+            }
+    },
+    "2021/03~06":{
+        "title":"Apprentice",
+        "content":"Google Cloud Student Sprint Program</br> This project will be a 10-week online course and workshop from March to June, and will be guided by Google mentor in groups. </br> The final goal is leading students to pass the Google Cloud Associate Cloud Engineer (ACE) professional certification exam.",
+        "button":{
+            "herf":"https://buildyourfuture.withgoogle.com/programs/cloud-student-sprint-programs/",
+            "content":"<i class='fa fa-google' aria-hidden='true'></i> details of this program "
+        }
+    },
+    "2020/11":{
+        "title":"Deputy convener/Speaker",
+        "content":" &quot;From Python to Tensorflow&quot; onsite bookclub</br>2020/11/10 ~ Now • Google Meet",
+        "button":{
+            "herf":"https://tensorflowbookclub.ml/",
+            "content":"<i class='material-icons'>menu_book</i> Join the bookclub "
+        }
+    },
+    "2020/10/17":{
+        "title":"DevFest 2020 Speaker",
+        "content":"host by Google Developer Group Taiwan , </br>In this online speech:<br>Share my experience in developing Google Assistant conversational Actions.<br>In the same time ,I take the audience to create a simple voice application as well.",
+        "button":{
+            "herf":"https://youtu.be/vA21dx5dPsE?t=3066",
+            "content":"<i class='fa fa-youtube-play' aria-hidden='true'></i> Live page"
+            }
+    },
+    "2020/Now":{
+        "title":"Study at <b>National Taiwan Unoversity of Science and Technology</b><i class='material-icons'>school</i>",
+        "content":"Computer Science and Information Engineering"
+    },
+    "2016/2018":{
+        "title":"Teach Assistant",
+        "content":"Undergraduate course :<br> Fundamental of Physics(I),Fundamental of Physics(II),Foundations of Materials Science and Engineering<br><br>1. Moodle management<br>2. Correcting homework and examination<br>3. As a invigilator/proctor<br>4. This position usually reserved for graduate students</br></br><small>(2016/02~2018/06，2 years and 5 months)</small>"
+    },
+    "2014/2015":{
+        "title":"Part-Time Worker",
+        "content":"National Chi Nan University Library</br> 1. Assistance in borrowing and returning books across libraries </br> 2. Assistance in library closure</br></br><small>(2014/11~2015/06，7 month)</small>"
+    },
+    "2013/2018":{
+        "title":"National Chi Nan University (Taiwan)<i class='material-icons'>school</i>",
+        "content":"Applied Material and Optoelectronic Engineering</br></br><small>(2013/09~2018/06)</small>"
+    },
+    "2014/2016":{
+        "title":"Manager",
+        "content":"The content of the position is: <br>- managing forums<br>- publishing or reorganizing resources. <br><br>During this tenure, I've been studied BB code (a kind of simplified HTML) to the extreme. <br>Bringing a simple and beautiful browsing and unified resources. <br>One of the links below is an example of the content as mentioned.",
+        "button":{
+            "herf":"https://apk.tw/thread-674690-1-1.html",
+            "content":"<i class='material-icons'>launch</i> Relative Page"
+            }
+    }
+};
