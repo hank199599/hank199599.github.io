54388cb23e1cbbc24631f0cdd93b86e7bccf67e4
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2021-06-24 12:05:27 +0800

    [Timeline] upload content

--- Timeline/generator.js
+++ Timeline/generator.js
@@ -7,7 +7,7 @@ for (var i=0;i<date_array.length;i++){
     substance=substance+'<h3 class="title">'+contents[date_array[i]].title+'</h3>';
     substance=substance+contents[date_array[i]].content;
     if(contents[date_array[i]].button!==undefined){
-        substance=substance+'<br>';
+        substance=substance+'<br><br>';
         substance=substance+'<a href="'+contents[date_array[i]].button.herf+'" target="_blank"><button class="btn btn-primary btn-round">'+contents[date_array[i]].button.content+'</button></a>'
     }                            
     substance=substance+'</li>'
