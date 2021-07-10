42a50f530cf391147c009fc886af068f621adfe5
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2021-07-10 12:13:18 +0800

    [Timeline] update content

--- en/Timeline/json.js
+++ en/Timeline/json.js
@@ -4,7 +4,23 @@ var contents={
         "content":"Woman in AI</br>This workshop will teach the AI ​​operating procedures from the perspective of a data scientist.<br> You don't need to have advanced programming skills. </br>In the workshop, senior elder sisters in the industry will serve as mentors and teaching assistants,<br>  leading everyone from basic data processing to how to convert data into computer-calculated data.",
         "button":{
             "herf":"https://www.accupass.com/event/2104250103576884283220",
-            "content":"<i class='material-icons'>menu_book</i> ACCUPASS "
+            "content":"<i class='material-icons'>menu_book</i>&nbsp;ACCUPASS "
+            }
+    },
+    "2021/07/10":{
+        "title":"Speaker",
+        "content":" TensorFlow Everywhere<br>This is a series of events organized by the leaders of the global TensorFlow and machine learning communities.",
+        "button":{
+            "herf":"https://www.meetup.com/en-AU/TensorFlow-User-Group-Taipei/events/277170902/",
+            "content":"<span class='btn-inner--icon'><span class='iconify' data-icon='cib:meetup' data-inline='false'></span>&nbsp;Meetup Page "
+            }
+    },
+    "2021/07~":{
+        "title":"Intern",
+        "content":"<i class='material-icons'>place</i><b>APMIC OpenTalk</b><br><small>the Preferred Brand for Digital Assistants in Digital Transformation OpenTalk</small>",
+        "button":{
+            "herf":"https://www.ap-mic.com/",
+            "content":"<i class='material-icons'>launch</i>&nbsp;relative page"
             }
     },
     "2021/03~06":{
@@ -12,7 +28,7 @@ var contents={
         "content":"Google Cloud Student Sprint Program</br> This project will be a 10-week online course and workshop from March to June, and will be guided by Google mentor in groups. </br> The final goal is leading students to pass the Google Cloud Associate Cloud Engineer (ACE) professional certification exam.",
         "button":{
             "herf":"https://buildyourfuture.withgoogle.com/programs/cloud-student-sprint-programs/",
-            "content":"<i class='fa fa-google' aria-hidden='true'></i> details of this program "
+            "content":"<i class='fa fa-google' aria-hidden='true'></i>&nbsp;details of this program "
         }
     },
     "2020/11":{
@@ -20,7 +36,7 @@ var contents={
         "content":" &quot;From Python to Tensorflow&quot; onsite bookclub</br>2020/11/10 ~ Now • Google Meet",
         "button":{
             "herf":"https://tensorflowbookclub.ml/",
-            "content":"<i class='material-icons'>menu_book</i> Join the bookclub "
+            "content":"<i class='material-icons'>menu_book</i>&nbsp;Join the bookclub "
         }
     },
     "2020/10/17":{
@@ -28,23 +44,27 @@ var contents={
         "content":"host by Google Developer Group Taiwan , </br>In this online speech:<br>Share my experience in developing Google Assistant conversational Actions.<br>In the same time ,I take the audience to create a simple voice application as well.",
         "button":{
             "herf":"https://youtu.be/vA21dx5dPsE?t=3066",
-            "content":"<i class='fa fa-youtube-play' aria-hidden='true'></i> Live page"
+            "content":"<i class='fa fa-youtube-play' aria-hidden='true'></i>&nbsp;Live page"
             }
     },
     "2020/Now":{
-        "title":"Study at <b>National Taiwan University of Science and Technology</b><i class='material-icons'>school</i>",
-        "content":"Computer Science and Information Engineering"
+        "title":"Study at <b>National Taiwan University of Science and Technology</b>",
+        "content":"Computer Science and Information Engineering",
+        "button":{
+            "herf":" https://www.ntust.edu.tw/index.php?Lang=en",
+            "content":"<i class='material-icons'>school</i>&nbsp;school page"
+            }
     },
     "2016/2018":{
         "title":"Teach Assistant",
-        "content":"Undergraduate course :<br> Fundamental of Physics(I),Fundamental of Physics(II),Foundations of Materials Science and Engineering<br><br>1. Moodle management<br>2. Correcting homework and examination<br>3. As a invigilator/proctor<br>4. This position usually reserved for graduate students</br></br><small>(2016/02~2018/06，2 years and 5 months)</small>"
+        "content":"<i class='material-icons'>place</i> <b>National Chi Nan University (Taiwan) </b><br>Undergraduate course :<br> Fundamental of Physics(I),Fundamental of Physics(II),Foundations of Materials Science and Engineering<br><br>1. Moodle management<br>2. Correcting homework and examination<br>3. As a invigilator/proctor<br>4. This position usually reserved for graduate students</br></br><small>(2016/02~2018/06，2 years and 5 months)</small>"
     },
     "2014/2015":{
         "title":"Part-Time Worker",
-        "content":"National Chi Nan University Library</br> 1. Assistance in borrowing and returning books across libraries </br> 2. Assistance in library closure</br></br><small>(2014/11~2015/06，7 month)</small>"
+        "content":"<i class='material-icons'>place</i> <b>National Chi Nan University (Taiwan) library</b></br> 1. Assistance in borrowing and returning books across libraries </br> 2. Assistance in library closure</br></br><small>(2014/11~2015/06，7 month)</small>"
     },
     "2013/2018":{
-        "title":"National Chi Nan University (Taiwan)<i class='material-icons'>school</i>",
+        "title":"<i class='material-icons'>school</i> National Chi Nan University (Taiwan)",
         "content":"Applied Material and Optoelectronic Engineering</br></br><small>(2013/09~2018/06)</small>"
     },
     "2014/2016":{
@@ -52,7 +72,7 @@ var contents={
         "content":"The content of the position is: <br>- managing forums<br>- publishing or reorganizing resources. <br><br>During this tenure, I've been studied BB code (a kind of simplified HTML) to the extreme. <br>Bringing a simple and beautiful browsing and unified resources. <br>One of the links below is an example of the content as mentioned.",
         "button":{
             "herf":"https://apk.tw/thread-674690-1-1.html",
-            "content":"<i class='material-icons'>launch</i> Relative Page"
+            "content":"<i class='material-icons'>launch</i>&nbsp;Relative Page"
             }
     }
 };
